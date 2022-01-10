dbd5fc42a2aa56ecee60637d0c343f8c02716165
Tanner <thetannerthomas@gmail.com>
2022-01-09 21:57:41 -0700

    add photo route

---
+++ src/routes/photos/Photos.tsx
@@ -0,0 +1,11 @@
+import React, { ReactElement } from "react";
+
+interface Props {}
+
+export function Photos({}: Props): ReactElement {
+  return (
+    <div>
+      <h1>Photos</h1>
+    </div>
+  );
+}
